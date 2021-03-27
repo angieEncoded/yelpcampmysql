b0912c841576d1d1fe5d5a5745a6b0618a1293fb
A.M. Krukovsky <akcoding2020@gmail.com>
2021-03-27 14:36:21 -0400

    Add seeding process

---
+++ seeds/index.js
@@ -0,0 +1,38 @@
+// This is a file to seed the campgrounds database in Mysql instead of mongodb
+require("dotenv").config();
+const Campground = require("../models/Campground.js");
+const LoremIpsum = require("lorem-ipsum").LoremIpsum;
+const cities = require("./cities.js");
+const { places, descriptors } = require("./seedHelpers");
+const sample = (array) => array[Math.floor(Math.random() * array.length)];
+const lorem = new LoremIpsum({
+  sentencesPerParagraph: {
+    max: 8,
+    min: 4,
+  },
+  wordsPerSentence: {
+    max: 16,
+    min: 4,
+  },
+});
+
+Campground.deleteAll()
+  .then(([rows, metadata]) => {
+    // add data back in
+    for (let i = 0; i < 50; i++) {
+      const price = parseFloat(
+        `${Math.floor(Math.random() * 99 + 1)}.${Math.floor(
+          Math.random() * 99 + 1
+        )}`
+      );
+      const random1000 = Math.floor(Math.random() * 1000);
+      const title = `${sample(descriptors)} ${sample(places)}`;
+      const description = lorem.generateSentences(5);
+      const location = `${cities[random1000].city}, ${cities[random1000].state}`;
+      const camp = new Campground(null, title, price, description, location);
+      camp.save();
+    }
+  })
+  .catch((error) => {
+    console.log(error);
+  });
